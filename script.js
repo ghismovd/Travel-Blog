ca52c4bf12f2b126999cc119d39cd86eab027b39
VlasieDragos <vlasiedragos1@gmail.com>
2017-03-27 01:54:57 +0300

    Content Update
    content-rotator, and post updates

---
+++ script.js
@@ -0,0 +1,43 @@
+var step = 25;
+var scrolling = false;
+
+// Wire up events for the 'scrollUp' link:
+$(".btn-nav-mp-top").bind("click", function(event) {
+    event.preventDefault();
+    // Animates the scrollTop property by the specified
+    // step.
+    $(".list-popular").animate({
+        scrollTop: "-=" + step + "px"
+    });
+}).bind("mouseover", function(event) {
+    scrolling = true;
+    scrollContent("up");
+}).bind("mouseout", function(event) {
+    scrolling = false;
+});
+
+
+$(".btn-nav-mp-bottom").bind("click", function(event) {
+    event.preventDefault();
+    $(".list-popular").animate({
+        scrollTop: "+=" + step + "px"
+    });
+}).bind("mouseover", function(event) {
+    scrolling = true;
+    scrollContent("down");
+}).bind("mouseout", function(event) {
+    scrolling = false;
+});
+
+function scrollContent(direction) {
+    var amount = (direction === "up" ? "-=1px" : "+=1px");
+    $(".list-popular").animate({
+        scrollTop: amount
+    }, 1, function() {
+        if (scrolling) {
+            scrollContent(direction);
+        }
+    });
+}
+
+
